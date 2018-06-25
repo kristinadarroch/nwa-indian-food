0ae31c6cffdab8c2677553c8ae12d4638e326921
kristinadarroch <dar15003@byui.edu>
2018-06-24 21:02:46 -0500

    images

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ const express = require('express');
 const app = express();
 
 app.set('port', (process.env.PORT || 3000))
-  .use(express.static(__dirname + '/public'))
+  .use(express.static(__dirname + '/assets'))
   .set('views', __dirname + '/views')
   .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index.ejs', {text: 'Hello World!'}))
+  .get('/', (req, res) => res.render('pages/index.ejs'))
   .listen(app.get('port'), () => {
     console.log('Node app running on port ', app.get('port'));
   });
