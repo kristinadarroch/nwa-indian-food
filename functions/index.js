afb64be1ce6f3090801572a5fb6a1746bc63be4b
coryswainston <swainston.cory89@gmail.com>
2018-07-01 21:40:02 -0500

    Add database and templating

--- functions/index.js
+++ functions/index.js
@@ -1,10 +1,24 @@
 const functions = require('firebase-functions');
-
+const firebase = require('firebase-admin');
 const express = require('express');
 const app = express();
 
+const fb = firebase.initializeApp(
+  functions.config().firebase
+);
+
 app.set('views', './views')
   .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index.ejs'));
+  .get('/', (req, res) => {
+    getReviews().then(reviews => {
+      res.render('pages/index.ejs', {reviews: reviews})
+    });
+  });
+
+
+function getReviews() {
+  const ref = fb.database().ref('reviews');
+  return ref.once('value').then(snap => snap.val());
+}
 
 exports.app = functions.https.onRequest(app);
